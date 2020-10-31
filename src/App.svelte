b83aa2376a1b894b80b9e074880d9926e24f5d46
Damodar Lohani <dlohani48@gmail.com>
2020-10-31 11:33:50 +0545

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+import Cta from "./components/CTA.svelte";
+import Features from "./components/Features.svelte";
+import Header from "./components/Header.svelte";
+</script>
+
+<main>
+	<Header />
+	<Features />
+	<Cta />
+</main>
+
+<style>
+
+</style>

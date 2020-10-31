2238e25d84901ff6b1da1b059d13dfe5fc782724
Damodar Lohani <dlohani48@gmail.com>
2020-10-31 11:54:49 +0545

    prettier

--- src/components/CTA.svelte
+++ src/components/CTA.svelte
@@ -3,24 +3,39 @@
   Read the documentation to get started: https://tailwindui.com/documentation
 -->
 <div class="bg-gray-50">
-    <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-      <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-        Ready to dive in?
-        <br>
-        <span class="text-pink-600">Start your free trial today.</span>
-      </h2>
-      <div class="mt-8 flex lg:flex-shrink-0 lg:mt-0">
-        <div class="inline-flex rounded-md shadow">
-          <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-pink-600 hover:bg-pink-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
-            Get started
-          </a>
+    <div
+        class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8
+        lg:flex lg:items-center lg:justify-between">
+        <h2
+            class="text-3xl leading-9 font-extrabold tracking-tight
+            text-gray-900 sm:text-4xl sm:leading-10">
+            Ready to dive in?
+            <br />
+            <span class="text-pink-600">Start your free trial today.</span>
+        </h2>
+        <div class="mt-8 flex lg:flex-shrink-0 lg:mt-0">
+            <div class="inline-flex rounded-md shadow">
+                <a
+                    href="#"
+                    class="inline-flex items-center justify-center px-5 py-3
+                    border border-transparent text-base leading-6 font-medium
+                    rounded-md text-white bg-pink-600 hover:bg-pink-500
+                    focus:outline-none focus:shadow-outline transition
+                    duration-150 ease-in-out">
+                    Get started
+                </a>
+            </div>
+            <div class="ml-3 inline-flex rounded-md shadow">
+                <a
+                    href="#"
+                    class="inline-flex items-center justify-center px-5 py-3
+                    border border-transparent text-base leading-6 font-medium
+                    rounded-md text-pink-600 bg-white hover:text-pink-500
+                    focus:outline-none focus:shadow-outline transition
+                    duration-150 ease-in-out">
+                    Learn more
+                </a>
+            </div>
         </div>
-        <div class="ml-3 inline-flex rounded-md shadow">
-          <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-pink-600 bg-white hover:text-pink-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
-            Learn more
-          </a>
-        </div>
-      </div>
     </div>
-  </div>
-  
+</div>

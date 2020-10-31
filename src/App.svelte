4232f16ddc85745339212f2739fa9de30aef0b0e
Damodar Lohani <dlohani48@gmail.com>
2020-10-31 12:15:01 +0545

    welcome message

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,5 @@
 <script>
-    import Cta from "./components/CTA.svelte";
-    import Features from "./components/Features.svelte";
-    import Header from "./components/Header.svelte";
+
 </script>
 
 <style>
@@ -9,7 +7,19 @@
 </style>
 
 <main>
-    <Header />
-    <Features />
-    <Cta />
+    <div class="w-full bg-pink-200">
+        <div class="container mx-auto text-center py-4">
+            <h1 class="font-bold text-2xl">Hello World!</h1>
+            Welcome to Svelte-Tailwind starter.
+            <div class="my-4">
+                <a
+                    target="_blank"
+                    class="p-2 bg-pink-600 hover:bg-pink-500 rounded text-white
+                    m-2"
+                    href="https://github.com/lohanidamodar/svelte-tailwind-starter">
+                    Star Github
+                </a>
+            </div>
+        </div>
+    </div>
 </main>
